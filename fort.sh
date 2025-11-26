0f20b290c3d56b4db53c55c15fb7a6c14df026c1
spe-lilmay <spewanyama@gmail.com>
2025-11-26 17:42:35 +0300

    fortnite

---
+++ fort.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "write your 1st number please"
+read num1
+echo "now write your 2nd number"
+read num2
+sum=$((num1 + num2))
+echo "and your answer iiiiiiis $sum you didn't know that"
+
